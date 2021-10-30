0d4f61d1c71345d3bef084d636f05f27dff92e48
plaurion1989 <plaurion@protonmail.com>
2021-10-29 17:31:22 -0700

    starting on the code for hashmap code challenge

---
+++ code-challenges/lib/src/main/java/codechallenges/hashtable/HashTablePair.java
@@ -0,0 +1,16 @@
+package lib.src.main.java.codechallenges.hashtable;
+
+import java.util.AbstractMap;
+
+public class HashTablePair<K, V> extends AbstractMap.SimpleEntry<K, V> implements Comparable<HashTablePair<K, V>>
+{
+    public HashTablePair(K key, V value)
+    {
+        super(key,value);
+    }
+
+    @Override
+    public int compareTo(HashTablePair<K, V> o) {
+        throw new UnsupportedOperationException("HashTablePair doesn't support comparison!");
+    }
+}
---
+++ code-challenges/lib/src/test/java/codechallenges/hashtable/HashTableTest.java
@@ -0,0 +1,12 @@
+package codechallenges.hashtable;
+import lib.src.main.java.codechallenges.hashtable.HashTable;
+import org.junit.jupiter.api.Test;
+
+public class HashTableTest {
+
+        @Test
+        void Test_hash_map()
+        {
+            HashTable<Integer, String> sut = new HashTable(10);
+        }
+    }

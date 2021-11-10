673f2350a2741ce3c76c931e4418eba4d27178b1
plaurion1989 <plaurion@protonmail.com>
2021-11-10 03:08:39 -0800

    still have some massaging to do with my kthFromEnd method, but basically works i suppose

--- code-challenges/lib/src/test/java/codechallenges/linkedlist/LinkedListTest.java
+++ code-challenges/lib/src/test/java/codechallenges/linkedlist/LinkedListTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedListTest {
-    LinkedList testList = new LinkedList();
+
 
     @Test void linkedListTest()
     {
+        LinkedList testList = new LinkedList();
         testList.insert(1);
         testList.insert(5);
         testList.insert(9);
@@ -24,18 +25,37 @@ public class LinkedListTest {
     }
     @Test void linkedListTest2()
     {
+        LinkedList testList = new LinkedList();
         testList.insert(1);
         testList.insert(4);
         testList.insert(7);
         // function is currently not placing newNode with new value into the linked list.
         // running in debug mode and can see my logic is stepping into where i want it to, it's just not
         // putting a new node anywhere in the LL.
-        testList.append(6);
-        testList.insertBefore(1,3);
+//        testList.append(6);
+//        testList.insertBefore(1,3);
 //        testList.insertAfter(3,2);
 
         System.out.println(testList.toString());
     }
+    @Test void linkedListTest3()
+    {
+        LinkedList testList = new LinkedList();
+        testList.insert(1);
+        testList.insert(2);
+        testList.insert(3);
+        testList.insert(4);
+        testList.insert(5);
+        LinkedList testList2 = new LinkedList();
+        testList2.insert(1);
 
+        testList.kthFromEnd(2);
+        testList.kthFromEnd(5);
+        testList.kthFromEnd(6); // looks like I need to make a few adjustments to handle int k being bigger than
+                                    // size of the LL.
+//        testList.kthFromEnd(-2); // test's lock up in potentially infinite loop?
+//        testList2.kthFromEnd(0); // test's lock up in potentially infinite loop?
+
+    }
 
 }

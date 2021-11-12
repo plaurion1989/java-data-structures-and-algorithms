9dfce00ca3c7aa889aeb7220d65c38d49fecfe58
plaurion1989 <plaurion@protonmail.com>
2021-11-11 16:43:24 -0800

    finished a few methods in binarySearchTree class

--- code-challenges/lib/src/main/java/codechallenges/tree/BinarySearchTree.java
+++ code-challenges/lib/src/main/java/codechallenges/tree/BinarySearchTree.java
@@ -2,14 +2,51 @@ package codechallenges.tree;
 
 public class BinarySearchTree<T extends Comparable<? super T>> extends BinaryTree<T>
 {
-    public void add(T value)
+    public Node<T> add(T value, Node<T> currentNode)
     {
-        // TODO: implement me
+        Node<T> nodeToAdd = new Node<>(value);
+        if(this.root == null)
+        {
+            this.root = nodeToAdd;
+            return nodeToAdd;
+        }
+        // when we reach past a targeted leaf, we return nodeToAdd at desired location
+        if (currentNode == null)
+        {
+            return nodeToAdd;
+        }
+        // if nodeToAdd.value > currentNode.value then we traverse to the right until nodeToAdd.value !> currentNode.value
+        if(nodeToAdd.value.compareTo(currentNode.value) > 0)
+        {
+                currentNode.rightNode = this.add(value, currentNode.rightNode);
+        }
+        // if nodeToAdd.value < currentNode.value then we traverse to the right until nodeToAdd.value !< currentNode.value
+        if(nodeToAdd.value.compareTo(currentNode.value) < 0)
+        {
+                currentNode.leftNode = this.add(value, currentNode.leftNode);
+        }
+        //redundant but necessary I think.
+        return nodeToAdd;
     }
 
-    public boolean contains (T value)
+    public boolean contains (T value, Node<T> currentNode)
     {
-        // TODO: implement me
+        // if value > currentNode.value then we traverse to the right until value !> currentNode.value
+        if(value.compareTo(currentNode.value) > 0)
+        {
+           return this.contains(value, currentNode.rightNode);
+        }
+        // if value < currentNode.value then we traverse to the right until value !< currentNode.value
+        if(value.compareTo(currentNode.value) < 0)
+        {
+           return this.contains(value, currentNode.leftNode);
+        }
+        // if root.value == value && currentNode.value == value
+        if(value.compareTo(currentNode.value) == 0)
+        {
+            return true;
+        }
+
         return false;
     }
 }

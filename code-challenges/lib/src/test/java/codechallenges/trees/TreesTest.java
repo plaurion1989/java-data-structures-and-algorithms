e7606d6668f8f946aed58981dfebdd577db03e3e
plaurion1989 <plaurion@protonmail.com>
2021-11-15 03:59:31 -0800

    something isnt right

---
+++ code-challenges/lib/src/test/java/codechallenges/trees/TreesTest.java
@@ -0,0 +1,39 @@
+package codechallenges.trees;
+import codechallenges.tree.BinarySearchTree;
+import codechallenges.tree.BinaryTree;
+import codechallenges.tree.Node;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TreesTest
+{
+    @Test void firstTest()
+    {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        int[] testArr = {1,2,3,4,5,6,7,8};
+        Node root = tree.arrToBBST(testArr);
+        Object[] testing = tree.preOrderTraversal(root);
+        Object[] test =  tree.inOrderTraversal(root);
+        Object[] tested = tree.postOrderTraversal(root);
+        for(int i=0; i<= testing.length -1; i++)
+        {
+            System.out.println("Node value added: " + testing[i]);
+            System.out.println("Node value added: " + test[i]);
+            System.out.println("Node value added: " + tested[i]);
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}

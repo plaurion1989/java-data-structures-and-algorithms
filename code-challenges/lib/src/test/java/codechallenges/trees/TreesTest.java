2cadcc6b83454310d032d2edd215496a06166bfd
plaurion1989 <plaurion@protonmail.com>
2021-11-15 04:10:44 -0800

    fixing random challenge mess ups

--- code-challenges/lib/src/test/java/codechallenges/trees/TreesTest.java
+++ code-challenges/lib/src/test/java/codechallenges/trees/TreesTest.java
@@ -12,21 +12,21 @@ public class TreesTest
 {
     @Test void firstTest()
     {
-        BinarySearchTree tree = new BinarySearchTree();
-
-        int[] testArr = {1,2,3,4,5,6,7,8};
-        Node root = tree.arrToBBST(testArr);
-        Object[] testing = tree.preOrderTraversal(root);
-        Object[] test =  tree.inOrderTraversal(root);
-        Object[] tested = tree.postOrderTraversal(root);
-        for(int i=0; i<= testing.length -1; i++)
-        {
-            System.out.println("Node value added: " + testing[i]);
-            System.out.println("Node value added: " + test[i]);
-            System.out.println("Node value added: " + tested[i]);
-
-
-        }
+//        BinarySearchTree tree = new BinarySearchTree();
+//
+//        int[] testArr = {1,2,3,4,5,6,7,8};
+//        Node root = tree.arrToBBST(testArr);
+//        Object[] testing = tree.preOrderTraversal(root);
+//        Object[] test =  tree.inOrderTraversal(root);
+//        Object[] tested = tree.postOrderTraversal(root);
+//        for(int i=0; i<= testing.length -1; i++)
+//        {
+//            System.out.println("Node value added: " + testing[i]);
+//            System.out.println("Node value added: " + test[i]);
+//            System.out.println("Node value added: " + tested[i]);
+//
+//
+//        }
 
 
 
